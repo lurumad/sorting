3afa42308660d0af298754cdf0ed4d19c838e47d
unknown <carlos.landeras@gmail.com>
2017-07-04 18:19:42 +0200

    Add Generic QuickSorting supporting custom IComparers and respective Tests

---
+++ src/Sorts/QuickSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorts
+{
+    public class QuickSort<T>
+    {
+        private IComparer<T> comparer = null;
+        public QuickSort(IComparer<T> comparer = null)
+        {
+            this.comparer = comparer ?? Comparer<T>.Default;
+        }
+        
+        private int Partition(T[] list, int left, int right, int pivotidx)
+        {
+            T pivot = list[pivotidx];
+            swap(list, right, pivotidx);
+
+            for (int i = left; i < right; i++)
+            {
+                if (comparer.Compare(list[i], pivot) < 0)
+                {
+                    swap(list, i, left);
+                    left++;
+                }
+            }
+
+            swap(list, left, right);
+            return left;
+        }
+
+        private void Sort(T[] list, int left, int right)
+        {
+            if (right > left)
+            {
+                int pivotidx = left;
+                pivotidx = Partition(list, left, right, pivotidx);
+                Sort(list, left, pivotidx);
+                Sort(list, pivotidx + 1, right);
+            }
+        }
+
+        private void swap(T[] list, int a, int b)
+        {
+            T temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+
+        public T[] Sort(T[] list)
+        {
+            Sort(list, 0, list.Length - 1);
+            return list;
+        }
+    }
+}
